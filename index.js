2bc4e88415a5d94cac65f185663f9568b261c785
Gapoortela <140081355+Gapoortela@users.noreply.github.com>
2023-07-25 21:19:24 -0300

    exer

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+
+
 console.log("hello word");
+
+///
+
+const prompt = require ("prompt-sync")();
+
+var nome = prompt ("insira seu nome ");
+var idade = prompt ("Insira idade ");
+var cor = prompt ("insira uma cor ");
+
+console.log(nome)
+console.log(idade)
+console.log(cor)
+
+/// Soma 
+
+var valor = prompt (" Insira o valor ");
+var valor2 = prompt (" Insira o valor");
+var valor3 = prompt (" Insira o valor ");
+var soma = valor + valor2 + valor3; 
+
+
+console.log(valor)
+console.log(valor2)
+console.log(valor3)
+console.log(`A soma dos valores é: ${soma}`)
+
 
